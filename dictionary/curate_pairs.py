9f98ec22fad11e55ea1d803027eb80c17db765ab
Soumendra Kumar Sahoo <soumendra.s@outlook.com>
2021-08-21 00:55:02 +0530

    file size reduced

--- dictionary/curate_pairs.py
+++ dictionary/curate_pairs.py
@@ -6,26 +6,30 @@
 """
 import json
 
-with open("./data/translated.txt") as fh:
-    word_pairs = fh.readlines()
-try:
-    output_json = {}
-    for word_pair in word_pairs:
-        word_pair = word_pair.rstrip(" |\n")
-        en_word, or_word = word_pair.split("||")
-        output_json[en_word] = or_word
-except Exception:
-    print(word_pair, word_pair.split("||"))
-# Serializing json
-json_object = json.dumps(output_json, indent=4, ensure_ascii=False)
+# with open("./data/translated.txt") as fh:
+#     word_pairs = fh.readlines()
+# try:
+#     output_json = {}
+#     for word_pair in word_pairs:
+#         word_pair = word_pair.rstrip(" |\n")
+#         en_word, or_word = word_pair.split("||")
+#         output_json[en_word] = or_word
+# except Exception:
+#     print(word_pair, word_pair.split("||"))
+# # Serializing json
+# json_object = json.dumps(output_json, indent=4, ensure_ascii=False)
 
-# Writing to sample.json
-with open("./data/cleaned_pairs.json", "w") as wfh:
-    wfh.write(json_object)
+# # Writing to sample.json
+# with open("./data/cleaned_pairs.json", "w") as wfh:
+#     wfh.write(json_object)
 
-# with open("./data/Odia_structured_wordlist.json", "r+") as fh:
-#     all_words = json.load(fh)
-#     for all_word in all_words:
-#         del all_words[all_word]['raw']
-#     all_words = json.dumps(all_words, indent=4, ensure_ascii=False)
-#     fh.write(all_words)
+with open("./data/Odia_structured_wordlist.json") as fh:
+    all_words = json.load(fh)
+
+write_words = {}
+for all_word in all_words:
+    if len(all_word.split(' ')) < 2:
+        write_words[all_word] = all_words[all_word]
+write_words = json.dumps(write_words, indent=4, ensure_ascii=False)
+with open("./data/Odia_structured_wordlist.json", "w+") as fh:
+    fh.write(write_words)
